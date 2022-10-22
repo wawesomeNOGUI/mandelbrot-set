1025237052e0d66e6f16427527273afc237ea931
Will Forcey <wsforc3y@gmail.com>
2022-10-22 15:29:39 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -33,11 +33,28 @@ const relativePoint = (x, y) => {
 }
 
 const mandelbrot = (c) => {
+    /*
     let z = { x: 0, y: 0 }, n = 0, p, d;
     do {
         p = {
             x: Math.pow(z.x, 2) - Math.pow(z.y, 2),
             y: 2 * z.x * z.y
+        }
+        z = {
+            x: p.x + c.x,
+            y: p.y + c.y
+        }
+        d = 0.5 * (Math.pow(z.x, 2) + Math.pow(z.y, 2))
+        n += 1
+    } while (d <= 2 && n < MAX_ITERATION)
+
+    return [n, d <= 2]
+    */
+    let z = { x: 0, y: 0 }, n = 0, p, d;
+    do {
+        p = {
+            x: Math.pow(z.x, 3) - 3 * z.x * Math.pow(z.y, 2),
+            y: 3 * Math.pow(z.x, 2) * z.y - Math.pow(z.y, 3)
         }
         z = {
             x: p.x + c.x,
